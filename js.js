3fdfe7e9821e159f9311ae082013ffb344ca1790
Yuri Morozov <99718748+Muskat90@users.noreply.github.com>
2022-10-09 20:07:24 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной */
+const button = document.querySelector('button');
+
+/* Обработчик на кнопке, запускает логику после клика */
+button.addEventListener('click', function() {
+	alert('Делай хорошо, а плохо не делай')
+})
